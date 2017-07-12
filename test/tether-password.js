82c89026e9ef0e28cbb5ec445e9835b552e3a4c8
bredele <olivier.wietrich@gmail.com>
2017-07-12 15:49:38 -0600

    Hash is not blank

--- test/tether-password.js
+++ test/tether-password.js
@@ -4,10 +4,18 @@
  */
 
 const test = require('tape')
+const alloc = require('buffer-alloc')
 const password = require('..')
 
 
 test('should generate buffer from string password', assert => {
   assert.plan(1)
   assert.equal(password('hello world') instanceof Buffer, true)
+})
+
+
+test('should not be blank', assert => {
+  assert.plan(1)
+  const hash = password('hello world')
+  assert.notEqual(hash, alloc(hash.length), 'not blank')
 })
